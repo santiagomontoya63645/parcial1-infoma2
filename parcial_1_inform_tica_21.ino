291f9661f70c7badb5806c7ab761baedc814bf00
santiagomontoya665 <62152388+santiagomontoya665@users.noreply.github.com>
2021-04-21 21:50:35 -0500

    segundo esquema
    hubo un fallo en nuestro primer esquema así que hemos decidido hacer otro esquema

--- parcial_1_inform_tica_21.ino
+++ parcial_1_inform_tica_21.ino
@@ -1,58 +1,52 @@
-/*
-SER ---> Entrada serial
-RLCK ---> Reloj de registro de salida
-SRCLK ---> Reloj de registro de desplazamiento
-*/
-const int SRCLK_1 = 5;
-const int RLCK_1 = 6;
-const int SER_1 = 7;
+int pinData  = 2;
+int pinLatch = 3;
+int pinClock = 4;
+#define TIEMPO 200
 
-const int SRCLK_2 = 2;
-const int RLCK_2 = 3;
-const int SER_2 = 4;
-
-char ingresado[30];
-int posicion=0;
-
-void verificacion()
-{
-  for(int i=0;i<8;i++){
-    digitalWrite(7,1);
-
-  	digitalWrite(5,0);
-  	digitalWrite(5,1);
-  	digitalWrite(5,0);
+void ledWrite(int aLed,int bLed,int cled,int dled,int eled,int fled,int gled,int hled){
   
-  	digitalWrite(6,0);
-  	digitalWrite(6,1);
-  	digitalWrite(6,0);
-  }
+   shiftOut(pinData, pinClock, LSBFIRST, aLed);
+   shiftOut(pinData, pinClock, LSBFIRST, bLed);
+   shiftOut(pinData, pinClock, LSBFIRST, cled);
+   shiftOut(pinData, pinClock, LSBFIRST, dled);
+   shiftOut(pinData, pinClock, LSBFIRST, eled);
+   shiftOut(pinData, pinClock, LSBFIRST, fled);
+   shiftOut(pinData, pinClock, LSBFIRST, gled);
+   shiftOut(pinData, pinClock, LSBFIRST, hled);
+   digitalWrite(pinLatch, HIGH);
+   digitalWrite(pinLatch, LOW);
 }
 
-void setup()
-{
-  // Configuración de los puertos seriales
-  for(int i=2;i<=7;i++){
-    pinMode(i,OUTPUT);
-    digitalWrite(i,0);
-  }
-  Serial.begin(9600);
-  Serial.println("Escriba 'test' si desea realizar la verificacion de los LED");
+void setup(){
+   pinMode(pinData, OUTPUT);
+   pinMode(pinLatch, OUTPUT);
+   pinMode(pinClock, OUTPUT);
 }
 
-void loop()
-{
-  if (Serial.available()){
-    while(Serial.available()>0) 
-    {
-      delay(5);
-      ingresado[posicion]=Serial.read();
-      posicion++;
-    }
-    Serial.print("Ingresado: ");
-  	Serial.println(ingresado);
-    if(ingresado[0]==116 && ingresado[1]==101 && ingresado[2]==115 && ingresado[3]==116){
-      verificacion();
-    }
-  }
+void loop(){
+  ledWrite(255,255,255,255,255,255,255,255);
+   //ledWrite(128,0,0); //delay(TIEMPO);
+  /* ledWrite(64,0,0);  delay(TIEMPO);
+   ledWrite(32,0,0);  delay(TIEMPO);
+   ledWrite(16,0,0);  delay(TIEMPO);
+   ledWrite(8,0,0);   delay(TIEMPO);
+   ledWrite(4,0,0);   delay(TIEMPO);
+   ledWrite(2,0,0);   delay(TIEMPO);
+   ledWrite(1,0,0);   delay(TIEMPO);
+   ledWrite(0,128,0); //delay(TIEMPO);
+   ledWrite(0,64,0);  delay(TIEMPO);
+   ledWrite(0,32,0);  delay(TIEMPO);
+   ledWrite(0,16,0);  delay(TIEMPO);
+   ledWrite(0,8,0);   delay(TIEMPO);
+   ledWrite(0,4,0);   delay(TIEMPO);
+   ledWrite(0,2,0);   delay(TIEMPO);
+   ledWrite(0,1,0);   delay(TIEMPO);
+   ledWrite(0,0,128); //delay(TIEMPO);
+   ledWrite(0,0,64);  delay(TIEMPO);
+   ledWrite(0,0,32);  delay(TIEMPO);
+   ledWrite(0,0,16);  delay(TIEMPO);
+   ledWrite(0,0,8);   delay(TIEMPO);
+   ledWrite(0,0,4);   delay(TIEMPO);
+   ledWrite(0,0,2);   delay(TIEMPO);
+   ledWrite(0,0,1);   delay(TIEMPO);*/
 }
